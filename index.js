11d8df54a127b9a683993977a817c4028e22c092
taizo-pro <62542724+taizo-pro@users.noreply.github.com>
2020-06-04 16:24:38 +0900

    complite
    It's a first time to upload as this App

---
+++ index.js
@@ -0,0 +1,22 @@
+const timer = document.querySelector("h2");
+
+const start = document.querySelector("start");
+const stop = document.querySelector("stop");
+const reset = document.querySelector("reset");
+
+//数字をカウントアップさせる機能
+
+//ボタンを押したらカウントアップスタートする
+
+//ボタンを押したらカウントアップをストップする
+
+//ボタンを押したらカウントをリセットする
+
+
+let nowDate = new Date();
+
+nowDate.getFullYear();
+
+nowDate.getMonth();
+
+nowDate.getDate();
